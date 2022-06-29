04b2dca21d12c8ed220f929dd23900ae2dc76dac
jiwon <owohjw@gmail.com>
2022-06-29 17:05:17 +0900

    정육각진행중

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new Swiper = new Swiper('.sw_visual', {
+  pagignation: {
+      el: ".swiper-pagiantion",
+      type: "fraction",
+
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+
+new Swiper = new Swiper(".sw_visual", {});
